abce41a3778b5005f743eda53cbc3d70770a0735
vagrant <rik.s.1005@gmail.com>
2020-11-28 14:52:32 +0000

    [update]画像投稿機能追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '2c5814e64f668e3fbf73ef5e6530ebd2cb8d2e784c946912e9f4601255c035a795e13312e470d3fe3a6efa5e188b00a0e1e9463e908cf4a99a8bfe3d5b01f65d'
